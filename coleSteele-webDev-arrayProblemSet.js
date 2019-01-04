b46329f44302b8c336acce9bb21e8748e424f390
Asha Holland <asha.holland@outlook.com>
2019-01-04 06:55:31 -0600

    Create coleSteele-webDev-arrayProblemSet.js

---
+++ coleSteele-webDev-arrayProblemSet.js
@@ -0,0 +1,38 @@
+
+/*
+My code solutions for the Cole Steele  - Web Developer Bootcamp: Array Problem Set 
+*/
+
+      // reversePrint() - it prints the array in reverse
+
+var array1 = [1,2,"A","B","C",5, 7, 13, 23]; 
+var newArr = [];
+var counter = 0; 
+
+
+        function printReverse(){ 
+
+        array1.forEach(function() {
+        newArr.unshift(array1[counter]);
+        counter++;
+
+        } );
+        console.log(newArr);  
+
+        }
+
+        printReverse();
+        
+       //  :) 
+
+
+      // uniform() - it checks if the values in the array are all the same or not
+
+
+
+      // sumArray() - it checks the values in an array of numbers and returns the sum of those numbers 
+
+
+      // max() - it returns the highest value in an array of numbers
+ 
+ 
